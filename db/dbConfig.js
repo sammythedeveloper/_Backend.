cd40aadf0665be6e1712429fc9844cf39e133afe
sammythedeveloper <justsammy484@gmail.com>
2024-02-16 01:41:24 -0500

    second commit

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,10 +1,10 @@
 const mysql2 = require("mysql2");
 
 const dbConnection = mysql2.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: "srv1102.hstgr.io",
+  user: "u775439782_forum",
+  password: "0911088006@Sa",
+  database: "u775439782_evanadi"
 });
 
 
